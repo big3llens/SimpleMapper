b6211272954357889e37cf35bcd528090f2ebd64
big3llens <zytgjvy.tuj1zytgjvy.tuj1>
2021-04-27 17:29:06 +0300

    add mapper

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -2,4 +2,28 @@ public class Product {
     private Long id;
     private String title;
     private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/main/java/ProductMapper.java
+++ src/main/java/ProductMapper.java
@@ -1,4 +1,7 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ProductMapper {
     private Connection connection;
@@ -6,4 +9,21 @@ public class ProductMapper {
     public ProductMapper(Connection connection) {
         this.connection = connection;
     }
+
+    public Product findById(Long idProduct) throws SQLException{
+        PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM PRODUCTS WHERE ID = ?");
+        statement.setLong(1,idProduct);
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setId(resultSet.getLong(1));
+                product.setTitle(resultSet.getString(2));
+                product.setPrice(resultSet.getInt(3));
+                return product;
+            }
+        }
+        return null;
+    }
+
 }
